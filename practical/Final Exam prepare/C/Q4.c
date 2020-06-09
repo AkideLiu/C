a021b7c1addd8c2d5c4f6fd39e87961d9da87e63
Akide Liu <mail@llycloud.com>
2020-06-09 23:55:02 +0930

    fix Final Exam prepare C part q4

--- practical/Final Exam prepare/C/Q4.c
+++ practical/Final Exam prepare/C/Q4.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
     char *array_row = (char *) malloc(sizeof(char) * 10);
@@ -21,10 +22,13 @@ int main(){
 
 
     for (int i = 0; i < count; ++i) {
+        array[i] = malloc(sizeof(char) * 10);
         printf("Enter the sequence: ");
         scanf("%s", array_row);
         scanf("%*[^\n]"); scanf("%*c");
-        array[i] = array_row;
+        for (int j = 0; j < strlen(array_row); ++j) {
+            array[i][j] = array_row[j];
+        }
     }
 
 
